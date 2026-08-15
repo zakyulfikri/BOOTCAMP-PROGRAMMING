3ded50c6919be9dd0efeebf916c7c011c74aa36a
zakyulfikri <mzakyulfikri@gmail.com>
2026-08-15 21:53:32 +0700

    update tugas sesi 15
    instalasi laravel breeze dan autentikasi login dan registrasi

---
+++ ecommerce-9/resources/views/components/application-logo.blade.php
@@ -0,0 +1,6 @@
+<!-- Menggunakan Teks/Icon Modern -->
+<div class="flex items-center space-x-2">
+    <span class="text-2xl font-black tracking-wider text-indigo-600">
+        MY<span class="text-gray-800">STORE</span>
+    </span>
+</div>
--- ecommerce-9/routes/web.php
+++ ecommerce-9/routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ProductsController;
 
-Route::get('/', [ShopController::class, 'home'])->name('home2');
-Route::get('/products', [ShopController::class, 'products'])->name('products.index');
-Route::get('/carts', [ShopController::class, 'carts'])->name('carts.index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
